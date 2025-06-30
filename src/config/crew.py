063a5a0576a51f04f1b0ec0d187370e166f9d5c1
Freedomtukun <167708652+Freedomtukun@users.noreply.github.com>
2025-06-30 16:01:03 -0500

    Create crew.py

---
+++ src/config/crew.py
@@ -0,0 +1,12 @@
+# src/config/crew.py
+
+from config import agents  
+from config import tasks   
+from crewai import Crew
+
+# This is a placeholder setup
+crew = Crew(
+    agents=[...],  
+    tasks=[...],   
+    verbose=True
+)
